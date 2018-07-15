e52ec653a665ef4f0e8309167ad304fc6207fba5
strike-fights <40926082+strike-fights@users.noreply.github.com>
2018-07-15 08:44:20 +0545

    Upload first assignment

---
+++ assignment.py
@@ -0,0 +1,18 @@
+v=25
+u=0
+t=10
+a=(v-u)/t
+print(a)
+f_name=input"(enter first name")
+l_name=input("enter last name")
+full name=f_name+" "+l_name
+print(full_name)
+
+#question 3
+user_input=(input("enter something")
+print(user_input +10)
+
+#question 4
+print(type(4))
+print(str(45))
+print(eval("2+3")
